de490a24318cf0844608386aa941e0741b46b1a3
Zipora5572 <z0548545572@gmail.com>
2025-05-14 15:24:10 +0300

    build project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Angular-App' title`, () => {
+  it(`should have the 'examease-admin' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Angular-App');
+    expect(app.title).toEqual('examease-admin');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Angular-App');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, examease-admin');
   });
 });
